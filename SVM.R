59e3e49289c01ef4c3e9e435bc058ab81acf0c93
Madhur Thakur <madhurthakur0608@gmail.com>
2025-01-01 21:03:57 -0600

    chang seed

--- SVM.R
+++ SVM.R
@@ -1,4 +1,4 @@
-set.seed(1)
+set.seed(2)
 x <- matrix(rnorm(20 * 2), ncol = 2)
 y <- c(rep(-1, 10), rep(1, 10))
 x[y == 1, ] <- x[y == 1, ] + 1
@@ -23,7 +23,7 @@ summary(svm.fit)
 
 ## Cross Validation to select optimum cost 
 
-set.seed(1)
+set.seed(2)
 
 tune.out <- tune(svm, y ~ . , data = dat, kernel = "linear",
      ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100))
