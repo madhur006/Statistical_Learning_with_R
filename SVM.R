7be64e1de2764a08e54b8a0c3e1c6cd52ea09ec0
Madhur Thakur <madhurthakur0608@gmail.com>
2025-01-02 21:16:30 -0600

    roc curves and multiple class

--- SVM.R
+++ SVM.R
@@ -146,4 +146,66 @@ table(
 )
 
 
+# ROC Curves 
 
+library(ROCR)
+
+
+rocplot <- function(pred, truth, ...) {
+  predob <- prediction(pred, truth)
+  perf <- performance(predob, "tpr", "fpr")
+  plot(perf, ...)
+}
+
+
+
+svm.fit.opt <- svm(y~., data = dat[train, ],
+                   kernel = "radial", gamma = 2, cost = 1, 
+                   decision.values = T)
+
+fitted <- attributes(
+  predict(svm.fit.opt, dat[train, ], decision.values = TRUE)
+)$decision.values
+
+fitted
+
+par(mfrow = c(1, 2))
+
+
+rocplot(-fitted, dat[train, "y"], main = "Training Data")
+
+# increase gamma 
+
+rocplot(-fitted, dat[train, "y"], main = "Training Data")
+svmfit.flex <- svm(y ~ ., data = dat[train, ], 
+                   kernel = "radial", gamma = 50, cost = 1, 
+                   decision.values = T)
+fitted <- attributes(
+  predict(svmfit.flex, dat[train, ], decision.values = T)
+)$decision.values
+rocplot(-fitted, dat[train, "y"], add = T, col = "red")
+
+
+fitted <- attributes(
+  predict(svmfit.opt, dat[-train, ], decision.values = T)
+)$decision.values
+rocplot(-fitted, dat[-train, "y"], main = "Test Data")
+fitted <- attributes(
+  predict(svmfit.flex, dat[-train, ], decision.values = T)
+)$decision.values
+rocplot(-fitted, dat[-train, "y"], add = T, col = "red")
+
+
+# SVM with Multiple Classes
+
+set.seed(1)
+x <- rbind(x, matrix(rnorm(50 * 2), ncol = 2))
+y <- c(y, rep(0, 50))
+x[y == 0, 2] <- x[y == 0, 2] + 2
+dat <- data.frame(x = x, y = as.factor(y))
+par(mfrow = c(1, 1))
+plot(x, col = (y + 1))
+
+svmfit <- svm(y ~ ., data = dat, kernel = "radial", 
+              cost = 10, gamma = 1)
+plot(svmfit, dat)
