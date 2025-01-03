7d96e617ccc2fd34b22757723f35bb70be71837e
Madhur Thakur <madhurthakur0608@gmail.com>
2025-01-02 19:53:47 -0600

    radial kernel

--- SVM.R
+++ SVM.R
@@ -1,3 +1,5 @@
+library(e1071)
+
 set.seed(1)
 x <- matrix(rnorm(20 * 2), ncol = 2)
 y <- c(rep(-1, 10), rep(1, 10))
@@ -6,7 +8,7 @@ plot(x, col = (3 - y))
 
 dat <- data.frame(x = x, y = as.factor(y))
 
-library(e1071)
+
 svm.fit <- svm(y~. , data = dat, kernel = "linear", 
     cost = 10, scale = FALSE)
 
@@ -82,5 +84,66 @@ plot(svm.fit2, dat)
 
 # Using cost = 1, we misclassify a training observation, but we also obtain a much wider margin and make use of seven support vectors. 
 # It seems likely that this model will perform better on test data than the model with cost = 1e5.
+
+
+### SVM - Non Linear Kernel - 
+# 1 polynomial  - use degree argument 
+# 2 radial - use gamma 
+
+
+# generate data 
+set.seed(1)
+x <- matrix(rnorm(200 * 2), ncol = 2)
+x[1:100, ] <- x[1:100, ] + 2
+x[101:150, ] <- x[101:150, ] - 2
+y <- c(rep(1, 150), rep(2, 50))
+dat <- data.frame(x = x, y = as.factor(y))
+
+plot(x, col = y)
+
+# split data 
+
+train <- sample(200, 100)
+svm.fit <- svm(y ~ ., data = dat[train, ], kernel = "radial",
+               gamma = 1 , cost = 1)
+
+plot(svm.fit, dat[train, ])
+
+summary(svm.fit)
+
+
+# increase cost 
+# increase cost : reduce misclassification errors
+# but irregular decision boundary increase risk of overfitting 
+
+svm.fit2 <- svm(y ~., data = dat[train, ], kernel = "radial",
+    gamma = 1, cost = 1e5)
+
+plot(svm.fit2, dat[train, ])
+
+summary(svm.fit2)
+
+
+# cross-validation using tune() to select the best choice of 𝛾and cost for an SVM with a radial kernel:
+
+set.seed(1)
+tune.out <- tune(svm, y~., data = dat[train, ],
+                 kernel = "radial", 
+                 ranges = list(
+                   cost = c(0.1, 1, 10, 100, 1000),
+                   gamma = c(0.5, 1, 2, 3, 4)
+                 ))
+
+summary(tune.out)
+
+# predict using best model 
+
+table(
+  true = dat[-train, "y"],
+  pred = predict(
+    tune.out$best.model, newdata = dat[-train, ]
+  )
+)
+
 
 
